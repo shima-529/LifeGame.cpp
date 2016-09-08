da6691abf564290d1957a8dbaf8a32c39694696f
shima-529 <simatyan@gmail.com>
2016-09-09 01:59:07 +0900

    modify the bug occurred by returning (-1) value

--- cellRW.cpp
+++ cellRW.cpp
@@ -7,18 +7,26 @@
 #include "h/extern"
 const int NOT_FOUND = -1;
 
-inline bool is_outOfRange(int param) {
+template<typename T>
+inline bool is_outOfRange(T param) {
 	return (param < 0 || param >= N) ? true : false;
 }
 
-int cellRead(int x, int y) {
-	if( is_outOfRange(x) )	return NOT_FOUND;
-	if( is_outOfRange(y) )	return NOT_FOUND;
+template<typename T>
+inline bool is_outOfRange(T p1, T p2) {
+	return is_outOfRange(p1) | is_outOfRange(p2);
+}
+
+bool cellRead(int x, int y) {
+	// This return value is added to the total neighbor-alive number.
+	// Therefore, returning (-1) occurs bugs. Boolean value is the most suitable.
+	if( is_outOfRange(x, y) )	return false;
+	// if( is_outOfRange(x, y) )	return NOT_FOUND;
+	// if( is_outOfRange(y) )	return NOT_FOUND;
 	return field[x][y].getStatus();
 }
 
-void cellWrite(int x, int y, int content) {
-	if( is_outOfRange(x) )	return;
-	if( is_outOfRange(y) )	return;
-	field[x][y].setStatus(content);
+void cellWrite(int x, int y, bool nextStatus) {
+	if( is_outOfRange(x, y) )	return;
+	field[x][y].setStatus(nextStatus);
 }
